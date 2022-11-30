2f535bd0e217054ab463e5fb72ce56a73db4e3f4
Samuel <71525496+Sam554-dev@users.noreply.github.com>
2022-11-30 20:31:51 -0300

    add comment

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth'
 import GithubProvider  from 'next-auth/providers/github'
 
+// authenticate with github
+
 export default NextAuth({
   providers: [
     GithubProvider({
